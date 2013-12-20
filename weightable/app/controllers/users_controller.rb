95f7c2284b460dbf12bd5898a547d0d09149c760
Tyler Wasden <twasden@gmail.com>
2013-12-20 14:57:41 -0600

    Created models and controllers

---
+++ weightable/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def updatecreate
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
