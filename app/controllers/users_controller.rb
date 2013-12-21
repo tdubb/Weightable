16a95aa83e02d0e0c395f13e745ff720b9941283
Tyler Wasden <twasden@gmail.com>
2013-12-21 15:18:52 -0600

    Cleaned up the link_to paths these will change once we get oauth working and have data in the db. right now they're just for show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def updatecreate
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+
+  end
+end
