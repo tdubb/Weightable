41f920eab8e8edaa2d7cd9d5bd273204dba58371
Qualquer Valor <qualquervalor@gmail.com>
2013-12-31 20:03:21 -0600

    Moved navbar into application.html.  Modify landing page to display app description

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def landing
-    redirect_to users_show_path(current_user)
+    if user_signed_in?
+      redirect_to users_show_path(current_user)
+    end
   end
+
   def new
   end
 
